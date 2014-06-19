cc1087171e3a5f1c7303e630df395107ab5752d1
chrisfunk <cfunk@secc.org>
2014-06-19 10:56:10 -0400

    DefinedValue updates and campus caching

--- org.secc.Rock.DataImport.BAL/RockMaps/DefinedValueMap.cs
+++ org.secc.Rock.DataImport.BAL/RockMaps/DefinedValueMap.cs
@@ -4,9 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using org.secc.Rock.DataImport.BAL.Controllers;
+using Rock.Model;
+
 namespace org.secc.Rock.DataImport.BAL.RockMaps
 {
     public class DefinedValueMap
     {
+        public RockService Service { get; set; }
+
+        private DefinedValueMap() { }
+
+        public DefinedValueMap( RockService service )
+        {
+            Service = service;
+        }
+
+        public int? Save( int definedTypeId, bool isSystem, string name, int order, string description = null, string foreignId = null, int? id = null )
+        {
+            DefinedValue dv;
+            DefinedValueController Controller = new DefinedValueController( Service );
+
+            if ( id == null )
+            {
+                dv = Controller.GetById( (int)id );
+
+                if ( dv == null )
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                dv = new DefinedValue();
+            }
+
+            throw new NotImplementedException();
+        }
     }
 }
