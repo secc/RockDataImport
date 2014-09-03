0bf5f736947e2f2977733b9dd0c28ea4d27d4c4e
chrisfunk <cfunk@secc.org>
2014-09-03 16:47:37 -0400

    defined type/value updates

--- org.secc.Rock.DataImport.BAL/Helper/DefinedTypeSummary.cs
+++ org.secc.Rock.DataImport.BAL/Helper/DefinedTypeSummary.cs
@@ -11,9 +11,13 @@ namespace org.secc.Rock.DataImport.BAL.Helper
     public class DefinedTypeSummary
     {
         public string Id { get; set; }
+        public string Category { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ForeignId { get; set; }
         public Guid UniqueIdentifier { get; set; }
+        public bool IsSystem { get; set; }
+
         public List<DefinedValueSummary> ValueSummaries { get; set; }
 
         public DefinedTypeSummary() { }
@@ -23,9 +27,12 @@ namespace org.secc.Rock.DataImport.BAL.Helper
             if ( dt != null )
             {
                 Id = dt.Id.ToString();
+                Category = dt.Category;
                 Name = dt.Name;
                 Description = dt.Description;
                 UniqueIdentifier = dt.Guid;
+                ForeignId = dt.ForeignId;
+                IsSystem = dt.IsSystem;
 
                 if(dt.DefinedValues != null && dt.DefinedValues.Count > 0)
                 {
@@ -34,8 +41,10 @@ namespace org.secc.Rock.DataImport.BAL.Helper
                                             Id = dv.Id.ToString(),
                                             DefinedTypeId = dv.DefinedTypeId.ToString(),
                                             Value = dv.Value,
+                                            Description = dv.Description,
+                                            ForeignId = dv.ForeignId,
                                             Order = dv.Order,
-
+                                            IsSystem = dv.IsSystem
                                         } ).ToList();
 
                 }
--- org.secc.Rock.DataImport.BAL/Helper/DefinedValueSummary.cs
+++ org.secc.Rock.DataImport.BAL/Helper/DefinedValueSummary.cs
@@ -10,7 +10,9 @@ namespace org.secc.Rock.DataImport.BAL.Helper
         public string Id { get; set; }
         public string DefinedTypeId { get; set; }
         public string Value { get; set; }
+        public string Description { get; set; }
         public string ForeignId { get; set; }
         public int Order { get; set; }
+        public bool IsSystem { get; set; }
     }
 }
