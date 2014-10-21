9bc855b6d10dabafe60b9ff10143a8ab09a031c0
chrisfunk <cfunk@secc.org>
2014-10-21 11:01:29 -0400

    Naming Updates

--- org.secc.Rock.DataImport/Properties/AssemblyInfo.cs
+++ org.secc.Rock.DataImport/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "org.secc.Rock.DataImport" )]
+[assembly: AssemblyTitle( "SECC Rock Data Import" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "org.secc.Rock.DataImport" )]
+[assembly: AssemblyProduct( "SECC Rock Data Import" )]
 [assembly: AssemblyCopyright( "Copyright ©  2014" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
